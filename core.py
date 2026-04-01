b589b3408d34e103b9169c7acaf60ac97bf6a331
Роман Недосеко <freelancerroma@mail.ru>
2026-04-01 19:58:13 +0300

    first upl

--- core.py
+++ core.py
@@ -1,9 +1,18 @@
 import pandas as pd
+from sklearn.model_selection import StratifiedKFold
 
 
-df_train = pd.read_csv('train.csv')
+
+DF_TRAIN = 'train.csv'
+DF_TEST = 'test.csv'
+
+df_train = pd.read_csv(DF_TRAIN)
 X_train = df_train.drop('Survived', axis=1)
 Y_train = df_train['Survived']
 
-df_test = pd.read_csv('test.csv')
+df_test = pd.read_csv(DF_TEST)
 Y_test = pd.read_csv('gender_submission.csv')['Survived'].values()
+
+
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
