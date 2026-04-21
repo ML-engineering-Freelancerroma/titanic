add750cdfcdce907939c108607d17c6b9d40cf3f
Роман Недосеко <freelancerroma@mail.ru>
2026-04-21 18:54:12 +0300

    +

--- core.py
+++ core.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from sklearn.model_selection import StratifiedKFold
+import numpy as np
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 
-DF_TRAIN = 'train.csv'
+DF_TRAIN = 'train_prep.csv'
 DF_TEST = 'test.csv'
 DF_SUB = 'gender_submission.csv'
 
@@ -11,7 +12,26 @@ X_train = df_train.drop('Survived', axis=1)
 Y_train = df_train['Survived']
 
 df_test = pd.read_csv(DF_TEST)
-Y_test = pd.read_csv(DF_SUB)['Survived'].values()
+# Y_test = pd.read_csv(DF_SUB)['Survived'].values()
 
 
 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+
+def evaluate_model(model, X, y, cv, scoring_dict, n_jobs=-1):
+    """
+    Универсальная функция для cross-validation
+    """
+
+    results = {}
+    for metric_name, scorer in scoring_dict.items():
+        scores = cross_val_score(
+            model,
+            X,
+            y,
+            cv=cv,
+            scoring=scorer,
+            n_jobs=n_jobs
+        )
+        results[metric_name] = float(np.mean(scores))
+    return results
